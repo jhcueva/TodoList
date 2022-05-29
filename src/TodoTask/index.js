f8e462c5845cc137494434f82f61a1c5404e2aed
jhcueva <humbertocueva97@gmail.com>
2022-05-29 15:03:27 -0500

    All bugs from TodoTask.js fixed

--- src/TodoTask/index.js
+++ src/TodoTask/index.js
@@ -12,6 +12,7 @@ function TodoTask() {
 
     const onClickInput = () => {
         console.log('onclick started')
+        document.getElementsByClassName('todoInput')[0].value = ''
         document.getElementsByClassName('writeIcon')[0].style.fontSize = 0
         document.getElementsByClassName('newtaskContainer')[0].classList.add('newtaskContainer--active')
         document.getElementById('taskInputContainer').classList.add('taskInputContainer--active')
@@ -21,25 +22,23 @@ function TodoTask() {
     }
 
     const onSubmit = (event) => {
-
         event.preventDefault();
         addTodo(newTodoValue)
+        onCloseTodo()
     }
     
     const onCloseTodo = () => {
-        document.getElementsByClassName('todoInput')[0].value = ''
         document.getElementsByClassName('writeIcon')[0].style.fontSize = ('1.4rem')
         document.getElementsByClassName('newtaskContainer')[0].className = ('newtaskContainer')
         document.getElementsByClassName('taskInputContainer--active')[0].className = ('taskInputContainer')
         document.getElementsByClassName('todoInput')[0].className = ('todoInput')
         document.getElementsByClassName('btnContainer')[0].style.display = 'none'
+        document.getElementsByClassName('todoInput')[0].value = ''
     }
     return (
         <>
             <h2>Create a new task</h2>
-            <section 
-                className='newtaskContainer'
-                >
+            <section className='newtaskContainer'>
                 <section className='taskInputContainer'
                     id='taskInputContainer'>
                     <form 
@@ -47,7 +46,7 @@ function TodoTask() {
                         onSubmit={onSubmit}>
                         <textarea
                             className='todoInput'
-                            value={newTodoValue}
+                            // value={newTodoValue}
                             onChange={onChange}
                             onClick={onClickInput}
                             placeholder="Launch rocket to the moon"
