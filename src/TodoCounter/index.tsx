932a81a69ebed769d6ad4ec0e13eedb2d4dc7a3e
jhcueva <humbertocueva97@gmail.com>
2022-07-28 17:55:26 -0500

    Confette animation added

--- src/TodoCounter/index.tsx
+++ src/TodoCounter/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './TodoCounter.css'
+import confetti from 'canvas-confetti'
 
 interface TodoCounterProps {
   totalTodos: number
@@ -9,11 +10,47 @@ interface TodoCounterProps {
 
 const TodoCounter: React.FC<TodoCounterProps> = ({ totalTodos, completedTodos, loading }) => {
 
+  var count = 200;
+  var defaults = {
+    origin: { y: 0.7 }
+  };
+
+  function fire(particleRatio, opts) {
+    confetti(Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio)
+    }));
+  }
+
   React.useEffect(() => {
     totalTodos - completedTodos > 0
       ? (document.title = `${totalTodos - completedTodos} ${totalTodos - completedTodos === 1 ? "pending task" : "pending tasks"
         }`)
       : (document.title = `No pending tasks`);
+
+    if ((totalTodos === completedTodos) && totalTodos !== 0) {
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+      });
+      fire(0.2, {
+        spread: 60,
+      });
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+      });
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+      });
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+      });
+    }
   }, [totalTodos, completedTodos]);
 
   return (
